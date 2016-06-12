37ba34620ed469a2299751fb27975d38c54f3da0
acuteMouse <1012592349@qq.com>
2016-06-12 19:07:02 +0800

    修改如果如果要创建的脚本名，存在，重复添加脚本头信息的BUG  要提交的变更： 	修改：     mkbash.sh

--- mkbash.sh
+++ mkbash.sh
@@ -1,8 +1,9 @@
-#! /bin/bash
+#! /bin/bash --login
 #作用：创建脚本时，自动添加头文件，
 #包含，作者，时间，版本信息。新创建的脚本作用需要自定义。
 #获取参数，即想要创建的脚本名，不需要自己添加后缀 .sh
 bashName=$1
+if [ ! -f "$bashName.sh" ];then
 echo "$1 is created now"
 #创建shell文件
 touch $bashName.sh
@@ -17,3 +18,6 @@ sed  '/$/a\\n' $bashName.sh
 echo "# @date:`date`">>$bashName.sh
 sed '/$/a\\n' $bashName.sh
 echo "# 作用：">>$bashName.sh
+else
+	echo "$bashName.sh is exited!"
+fi
