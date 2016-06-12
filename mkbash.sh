3dbd656b520214316c27a859164353f701a5528f
acuteMouse <1012592349@qq.com>
2016-06-12 19:19:29 +0800

    修改创建脚本时，不再输出自动创建的内容 不再添加换行符  要提交的变更： 	修改：     mkbash.sh

--- mkbash.sh
+++ mkbash.sh
@@ -3,6 +3,7 @@
 #包含，作者，时间，版本信息。新创建的脚本作用需要自定义。
 #获取参数，即想要创建的脚本名，不需要自己添加后缀 .sh
 bashName=$1
+#判断脚本想创建的脚本是否存在
 if [ ! -f "$bashName.sh" ];then
 echo "$1 is created now"
 #创建shell文件
@@ -10,13 +11,11 @@ touch $bashName.sh
 #定义shell类型，采用bash
 echo "# !/bin/bash">>$bashName.sh
 #换行
-sed '/$/a\\n' $bashName.sh
 # 定义作者,默认采用当前登陆用户
 echo "# @author: `who|cut -d ' ' -f1`">>$bashName.sh
-sed  '/$/a\\n' $bashName.sh
 # 时间
 echo "# @date:`date`">>$bashName.sh
-sed '/$/a\\n' $bashName.sh
+#作用
 echo "# 作用：">>$bashName.sh
 else
 	echo "$bashName.sh is exited!"
